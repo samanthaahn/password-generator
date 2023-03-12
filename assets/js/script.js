92291061a2276c6331f97bf3d3bbdaf6ccf23ec1
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-03-11 20:33:52 -0800

    added comments to my css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// I need a button to generate a password 
+// I need a button to generate a password DONE
 // Then I need a series of prompts for a password
 // I need criteria for the password like : length 8 characters minumium and no more than 128 characters 
 // I need to confrim whetehr or not to include lowercase, uppercase, numeric, and/or special characters when I answer each prompt 
@@ -19,11 +19,19 @@ var userChoice = '';
 
 // Write password to the #password input
 function writePassword() {
-var password = generatePassword();
+var password = generatePassword(); //function call SO we have to define it 
 var passwordText = document.querySelector("#password");
 
 passwordText.value = password;
 
+}
+
+function generatePassword() {
+    //ask user how many characters they want 
+    
+    //ask user what they want in the password 
+    //randomize the password 
+    //return randomizes password (this doesn't really count)
 }
 
 // Add event listener to generate button
