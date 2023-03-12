017d5df4df432dadad3470faa3c5f552646e4162
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-03-11 19:32:53 -0800

    Added all of the devlopment tools that were given to us into my code

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// I need a button to generate a password 
+// Then I need a series of prompts for a password
+// I need criteria for the password like : length 8 characters minumium and no more than 128 characters 
+// I need to confrim whetehr or not to include lowercase, uppercase, numeric, and/or special characters when I answer each prompt 
+// Input should be validated and at lease one character type should be selected 
+// When all prompts are answered then a password if generated that matches what the user picked. 
+// password is desplayed in an alert or written on the page 
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
