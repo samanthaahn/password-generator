0001064a22d315b2311a580ca8f84853f43315b2
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-03-11 21:04:38 -0800

    added prompts for lowercase, upper, special, and number:

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,19 +29,30 @@ passwordText.value = password;
 function generatePassword() {
     //ask user how many characters they want 
         // we are going to use the prompts and boundary of 8-128 characters DONE 
-        // we need an "if" statement to make sure it's valid 
-        // send an alert to use saying invalude responce 
-        // recall current function  
+        // we need an "if" statement to make sure it's valid DONE 
+        // send an alert to use saying invalude responce DONE 
+        // recall current function  DONE 
             // we need to save their response to their "length" variable 
     length = prompt("How long do you want your password to be? (Choose a number between 8 and 128");
     if (length < 8 || length > 128) {
         alert("Invalid responce!");
         generatePassword();
     }
-    
 
     //ask user what they want in the password 
+    // prompt user do you want lower case
+    lowerCase = prompt("Do you want lower case?");
+    // prompt do you want upper case
+    upperCase = prompt("Do you want upper case?");
+    // promopt do you want special characters 
+    specialCharacters = prompt("Do you want special characters?");
+    // promot do you want numbers
+    numbers = prompt("Do you want numbers? ");
+
+
     //randomize the password 
+
+
     //return randomizes password (this doesn't really count)
 }
 
